333a0131b4803975400da63dffc37484976b8ceb
cql <442510893@qq.com>
2019-11-15 15:59:34 +0800

    1.测试编辑Demo

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -9,6 +9,7 @@ public class Demo2Application {
     public static void main(String[] args) {
         SpringApplication.run(Demo2Application.class, args);
         System.out.println("简易springBoot项目搭建成功啦!!");
+        System.out.println("git编辑成功");
     }
 
 }
