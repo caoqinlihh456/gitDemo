0bb7fa689935e502f1e0ee035acd0364b0814a30
cql <442510893@qq.com>
2019-11-15 15:54:18 +0800

    1.测试Demo

---
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -0,0 +1,14 @@
+package com.example.demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2Application.class, args);
+        System.out.println("简易springBoot项目搭建成功啦!!");
+    }
+
+}
